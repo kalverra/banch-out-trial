05e4c790e522bf9cbfe211ccb86fb12c1ad9aff0
Adam Hamrick <adam.hamrick@smartcontract.com>
2025-07-14 19:35:22 -0400

    branch-out quarantine tests
    simple/simple_test.go: TestFlakySeventyFivePercent

--- simple/simple_test.go
+++ simple/simple_test.go
@@ -26,11 +26,6 @@ func TestSkip(t *testing.T) {
 }
 
 func TestFlakyTenPercent(t *testing.T) {
-	if os.Getenv("RUN_QUARANTINED_TESTS") != "true" {
-		t.Skip("Flaky test quarantined. Ticket <Jira ticket>. Done automatically by branch-out (https://github.com/smartcontractkit/branch-out)")
-	} else {
-		t.Logf("'RUN_QUARANTINED_TESTS' set to '%s', running quarantined test", os.Getenv("RUN_QUARANTINED_TESTS"))
-	}
 	t.Parallel()
 
 	rand := rand.Intn(100)
@@ -67,6 +62,11 @@ func TestFlakyFiftyPercent(t *testing.T) {
 }
 
 func TestFlakySeventyFivePercent(t *testing.T) {
+	if os.Getenv("RUN_QUARANTINED_TESTS") != "true" {
+		t.Skip("Flaky test quarantined. Ticket <Jira ticket>. Done automatically by branch-out (https://github.com/smartcontractkit/branch-out)")
+	} else {
+		t.Logf("'RUN_QUARANTINED_TESTS' set to '%s', running quarantined test", os.Getenv("RUN_QUARANTINED_TESTS"))
+	}
 	t.Parallel()
 
 	rand := rand.Intn(100)
